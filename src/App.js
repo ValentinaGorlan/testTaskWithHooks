2af1a61d086db363e40e6afb0805f35eb12ebbd2
ValentinaGorlan <valentina.gorlanova.ru@gmail.com>
2022-10-07 02:35:18 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -8,71 +8,79 @@ import { type } from "@testing-library/user-event/dist/type";
 
 function App() {
   const [term, setTerm] = useState("");
-  const [index, setIndex] = useState(0);
-  const [dark, setDark] = useState(null)
-  const [display, setDisplay] = useState('none')
+  const [index, setIndex] = useState(-1);
+  const [dark, setDark] = useState(null);
+  const [display, setDisplay] = useState("none");
 
   const onSearch = (term) => {
     setTerm(term);
   };
 
   const findIndex = (index) => {
-    setIndex(index)
-  }
+    setIndex(index);
+  };
 
   const onBack = (index) => {
-    setIndex(-1)
-  }
+    setIndex(-1);
+  };
 
   const searchImg = (items, term) => {
     if (term.length === 0) {
       return items;
     }
 
-    return items.filter(item => {
+    return items.filter((item) => {
       return item.name.toLowerCase().indexOf(term.toLowerCase()) > -1;
     });
-  }
+  };
 
   const openSettings = () => {
-    display == 'none'? setDisplay('block') : setDisplay('none');
-  }
+    display == "none" ? setDisplay("block") : setDisplay("none");
+  };
 
   return (
     <div className={dark ? "app_wrapper dark" : "app_wrapper"}>
       <div className="menu_wrapper">
         <div className="btn_container">
-          <button className="prev_btn"
-          style={{'display' : index > -1 ? 'block' : 'none'}}
-            onClick={onBack}>
-              <img src={require("./resourses/arrow.png")} alt="prev" />
-              Voltar
-            </button>
+          <button
+            className="prev_btn"
+            style={{ display: index > -1 ? "block" : "none" }}
+            onClick={onBack}
+          >
+            <img src={require("./resourses/arrow.png")} alt="prev" />
+            Voltar
+          </button>
         </div>
-        <SearchPanel onSearch={onSearch}/>
+        <SearchPanel onSearch={onSearch} />
         <div className="btn_container">
-          <button 
-          className="theme_btn"
-          onClick={openSettings}>
+          <button className="theme_btn" onClick={openSettings}>
             <img src={require("./resourses/settings.png")} alt="Change theme" />
           </button>
-        <div style={{'display': display}} className="theme_modal">
-          <p>Modo Dark</p>
-          <input 
-              type="checkbox" 
+          <div style={{ display: display }} className="theme_modal">
+            <p>Modo Dark</p>
+            <input
+              type="checkbox"
               id="highload1"
               name="highload1"
               className="setting_btn"
               onClick={() => {
-                setDark(!dark)
-                }
-              }
-              />
-              <label for="highload1" data-onlabel="Ativada" data-offlabel="Desativado" class="lb1"></label>
-        </div>
+                setDark(!dark);
+              }}
+            />
+            <label
+              for="highload1"
+              data-onlabel="Ativada"
+              data-offlabel="Desativado"
+              class="lb1"
+            ></label>
+          </div>
         </div>
       </div>
-      {index > -1 ? <MySlider data={Data()} ind={index}/> : <Album data={() => searchImg(Data(), term)} findIndex={findIndex}/>}
+      {index > -1 ? (
+        <MySlider data={Data()} ind={index} />
+      ) : (
+        <Album data={() => searchImg(Data(), term)} findIndex={findIndex} />
+      )}
     </div>
   );
 }
